7916245c8c366896e06cd6b73d5c1faa46c59bb0
Billy Zhu <zyx.billy@gmail.com>
2020-05-02 13:09:12 -0700

    add more typing annotations

--- secret_hitler/game.py
+++ secret_hitler/game.py
@@ -2,10 +2,12 @@
 
 Main game handle. Provides an external interface for the game.
 """
+from typing import Dict, Optional, Tuple
 
+from secret_hitler import stages
 from secret_hitler.board import Board
 from secret_hitler.exceptions import GameError
-from secret_hitler import stages
+from secret_hitler.prompts import Prompt
 
 
 class Game:
@@ -21,25 +23,25 @@ class Game:
         if self.stage is not None:
             raise GameError(f"Requires game to not have begun to {attempt}")
 
-    def get_full_state(self):
+    def get_full_state(self) -> Dict:
         self.requires_game_started("get the full state")
         return self.board.get_full_state()
 
-    def get_identity(self, name):
+    def get_identity(self, name) -> str:
         self.requires_game_started("get player identity")
         return self.board.get_player(name).identity.value
 
-    def add_player(self, name: str):
+    def add_player(self, name: str) -> None:
         self.requires_game_not_started("add a player")
         self.board.add_player(name)
 
-    def begin_game(self):
+    def begin_game(self) -> Tuple[Dict[str, Prompt], Dict]:
         self.requires_game_not_started("begin game")
         self.board.begin_game()
         self.stage = stages.RevealIdentities(self.board)
-        return (self.stage.prompts().get_dict(), None)
+        return (self.stage.prompts().get_dict(), self.board.extract_updates())
 
-    def perform_action(self, action, choice):
+    def perform_action(self, action, choice) -> Tuple[Optional[Dict[str, Prompt]], Dict]:
         next_stage = self.stage.perform_action(action, choice)
         if next_stage == self.stage:
             # this stage not done yet
--- secret_hitler/prompts.py
+++ secret_hitler/prompts.py
@@ -3,7 +3,7 @@
 A Prompt encapsulates a request for user action, along with the available choices the user can respond with.
 """
 
-from typing import Callable, List
+from typing import Callable, Dict, List
 
 from secret_hitler.player import Player
 
@@ -20,10 +20,10 @@ class Prompt:
 
 class Prompts:
     def __init__(self):
-        self.prompts = dict()
+        self.prompts: Dict[str, Prompt] = dict()
 
-    def add(self, player: Player, method: Callable, prompt_str: str, choices: List[str]):
+    def add(self, player: Player, method: Callable, prompt_str: str, choices: List[str]) -> None:
         self.prompts[player.name] = Prompt(method.__name__, prompt_str, choices)
 
-    def get_dict(self):
+    def get_dict(self) -> Dict[str, Prompt]:
         return self.prompts
